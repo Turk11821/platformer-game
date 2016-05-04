d2768be184a4d966de8dac980324037da72491a9
Turk11821 <jaymesturci@hotmail.com>
2016-05-04 18:36:34 +0930

    added index.html, main.js, player.js, keyboard.js, hero.png and tileset.png
    created a new project

---
+++ main.js
@@ -0,0 +1,119 @@
+var canvas = document.getElementById("gameCanvas");
+var context = canvas.getContext("2d");
+ 
+var startFrameMillis = Date.now();
+var endFrameMillis = Date.now();
+ 
+ 
+function getDeltaTime()
+{
+    endFrameMillis = startFrameMillis;
+    startFrameMillis = Date.now();
+ 
+    var deltaTime = (startFrameMillis - endFrameMillis) * 0.001;
+ 
+    if(deltaTime > 1)
+        deltaTime = 1;
+ 
+    return deltaTime;
+}
+ 
+ 
+var SCREEN_WIDTH = canvas.width;
+var SCREEN_HEIGHT = canvas.height;
+ 
+ 
+var fps = 0;
+var fpsCount = 0;
+var fpsTime = 0;
+ 
+var chuckNorris = document.createElement("img");
+chuckNorris.src = "hero.png";
+ 
+var player = new Player();
+var keyboard = new Keyboard();
+ 
+var LAYER_COUNT = 2;
+var MAP = { tw: 60, th: 15 };
+var TILE = 35;
+var TILESET_TILE = TILE * 2;
+var TILESET_PADDING = 2;
+var TILESET_SPACING = 2;
+var TILESET_COUNT_X = 14;
+var TILESET_COUNT_Y = 14;
+ 
+var tileset = document.createElement("img");
+tileset.src = "tileset.png";
+ 
+ 
+function drawMap()
+{
+    for(var layerIdx = 0; layerIdx < LAYER_COUNT; layerIdx++)
+    {
+        var idx = 0;
+        for(var y = 0; y < level1.layers[layerIdx].height; y++)
+        {
+            for(var x = 0; x < level1.layers[layerIdx].width; x++)
+            {
+                if(level1.layers[layerIdx].data[idx] != 0)
+                {
+                    var tileIndex = level1.layers[layerIdx].data[idx] - 1;
+                    var sx = TILESET_PADDING + (tileIndex % TILESET_COUNT_X) * (TILESET_TILE + TILESET_SPACING);
+                    var sy = TILESET_PADDING + (Math.floor(tileIndex / TILESET_COUNT_Y)) * (TILESET_TILE + TILESET_SPACING);
+                    context.drawImage(tileset, sx, sy, TILESET_TILE, TILESET_TILE, x*TILE, (y-1)*TILE, TILESET_TILE,TILESET_TILE);
+                }
+ 
+                idx++;
+            }
+        }
+    }
+}
+ 
+ 
+ 
+function run()
+{
+    context.fillStyle = "#ccc";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+ 
+    var deltaTime = getDeltaTime();
+ 
+    player.update(deltaTime);
+    player.draw();
+ 
+    fpsTime += deltaTime;
+    fpsCount++;
+    if(fpsTime >= 1)
+    {
+        fpsTime -= 1;
+        fps = fpsCount;
+        fpsCount = 0;
+    }
+ 
+    context.fillStyle = "#f00";
+    context.font = "14px Arial";
+    context.fillText("FPS: " + fps, 5, 20, 100);
+}
+ 
+(function() {
+  var onEachFrame;
+  if (window.requestAnimationFrame) {
+    onEachFrame = function(cb) {
+      var _cb = function() { cb(); window.requestAnimationFrame(_cb); }
+      _cb();
+    };
+  } else if (window.mozRequestAnimationFrame) {
+    onEachFrame = function(cb) {
+      var _cb = function() { cb(); window.mozRequestAnimationFrame(_cb); }
+      _cb();
+    };
+  } else {
+    onEachFrame = function(cb) {
+      setInterval(cb, 1000 / 60);
+    }
+  }
+ 
+  window.onEachFrame = onEachFrame;
+})();
+ 
+window.onEachFrame(run);
